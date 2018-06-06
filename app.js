0d08aed81b6ebedf08bf4c66a39e3c3f082aa782
Noelia Sabando <lolia_15@hotmail.com>
2018-06-06 15:02:05 -0400

    ejercicios arreglos

---
+++ app.js
@@ -0,0 +1,18 @@
+function computeAverageLengthOfWords (palabra1, palabra2) {
+   console.log((palabra1.length + palabra2.length)/2);
+}
+  
+function getNthElement(lista, entero){
+    return (lista[entero]);
+}
+
+function areValidCredentials(name, password) {
+    if(name.length >3 && password.length >= 8){
+        return true
+      }else {
+        return false
+      }
+  }
+  
+   
+
